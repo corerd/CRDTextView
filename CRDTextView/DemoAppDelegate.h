b68df58c8c9ffc4e6999048ba1f02d97c75663c6
corerd <corerd@live.com>
2013-05-30 02:53:56 +0200

    Initial Commit

---
+++ CRDTextView/DemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DemoAppDelegate.h
+//  CRDTextView
+//
+//  Created by Corrado Ubezio on 30/05/13.
+//  Copyright (c) 2013 Corerd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CRDTextView/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  CRDTextView
+//
+//  Created by Corrado Ubezio on 30/05/13.
+//  Copyright (c) 2013 Corerd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+@end
